0fb2f5fc9b89807787b86d27cdbb86eae3eaa5fa
Alistair Burrowes <alistair.burrowes@gmail.com>
2019-05-25 09:09:51 +1000

    typo

--- src/Type.hs
+++ src/Type.hs
@@ -5,7 +5,7 @@ module Type where
 {-
 this is really nicely implemented! I like this idea.
 
-I have thought about this a fair bit in regards to having the directouns as
+I have thought about this a fair bit in regards to having the directions as
 Bounded for my toy robot. I decided I didn't feel comfortable with it because I
 don't think it is correct to say a certain direction is the maximum or minimum
 direction. Bounded is mostly used for numbers like Int32 which have a clear
